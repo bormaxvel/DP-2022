011dde33a0cf17f026bf4d91ab41790d9bd089dc
Borysman <90153047+bormaxvel@users.noreply.github.com>
2022-02-01 17:32:09 +0200

    v0.00001 pre alpha

---
+++ Lab1/src/main/java/Gamepads/GamepadsClass.java
@@ -0,0 +1,15 @@
+package Gamepads;
+
+public class GamepadsClass {
+	String title;
+	int age;
+	float width;
+	
+	public GamepadsClass(String title, int age, float width) {
+		super();
+		this.title = title;
+		this.age = age;
+		this.width = width;
+	}
+	
+}
