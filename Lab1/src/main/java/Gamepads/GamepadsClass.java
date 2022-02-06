19dd83b589049adbd38518e089234743ed24a0db
Borysman <90153047+bormaxvel@users.noreply.github.com>
2022-02-06 12:04:22 +0200

    Server v0.1

--- Lab1/src/main/java/Gamepads/GamepadsClass.java
+++ Lab1/src/main/java/Gamepads/GamepadsClass.java
@@ -2,14 +2,43 @@ package Gamepads;
 
 public class GamepadsClass {
 	String title;
-	int age;
-	float width;
+	int price;
+	float weight;
 	
-	public GamepadsClass(String title, int age, float width) {
+	public GamepadsClass(String title, int price, float weight) {
 		super();
 		this.title = title;
-		this.age = age;
-		this.width = width;
+		this.price = price;
+		this.weight = weight;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	@Override
+	public String toString() {
+		return "{\"title\": \""+title+"\", \"price\":"+ price+", \"weight\":" + weight+"}";
 	}
 	
 }
---
+++ Lab1/src/main/java/servlet/Servlet1.java
@@ -0,0 +1,32 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import Gamepads.GamepadsClass;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		GamepadsClass Gamepads = new GamepadsClass("Gamepad1", 300, 0.3f);
+		out.println("["+Gamepads+"]");
+		
+	}
+
+}
