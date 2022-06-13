6d8d61f9e6d0d185f16d4950be1e15e49e7f4f2f
Borysman <90153047+bormaxvel@users.noreply.github.com>
2022-06-13 20:46:37 +0300

    123

---
+++ Lab1f/tables.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE student3_dp2022;
+create user bormaxv with password '1';
+grant all on database student3_dp2022 to bormaxv;
+CREATE TABLE "items" (
+    "id" serial PRIMARY KEY,
+    "name" varchar,
+    "price" int,
+    "description" varchar,
+    "link" varchar
+);
+CREATE TABLE "amount" (
+    "id" serial PRIMARY KEY,
+    "itemid" INT UNIQUE REFERENCES items(id),
+    "amount" int,
+    "description" varchar
+);
